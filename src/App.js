5839d5756581410c893ba477430dc29aaf90efff
Surajv <vsuraj2405@gmail.com>
2021-04-01 20:16:18 +0530

    testing navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1> hello </h1>
+      <Navbar />
     </div>
   );
 }
