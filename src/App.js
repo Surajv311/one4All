b6327b9ffdf43211f5085f7d6dd95da34cdbbbf4
Surajv <vsuraj2405@gmail.com>
2021-04-02 21:23:37 +0530

    unifying components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import React from "react";
+import Table from "./components/Table";
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <header class="font-serif">
+        <h1>oneForAll (One point for all resources and opportunities)</h1>
+      </header>
+
+      <Intro />
+      <Table />
+
+      <Footer />
     </div>
   );
 }
