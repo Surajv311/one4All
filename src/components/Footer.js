dbc14e9890cccacc25b0279127739fb8c3327799
Surajv <vsuraj2405@gmail.com>
2021-04-02 22:26:09 +0530

    removed comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,33 +18,4 @@ function Footer() {
   );
 }
 
-// export default Footer;
-// var style = {
-//   backgroundColor: "black",
-//   borderTop: "1px solid #E7E7E7",
-//   textAlign: "center",
-//   padding: "20px",
-//   position: "fixed",
-//   left: "0",
-//   bottom: "0",
-//   height: "60px",
-//   width: "100%",
-// };
-
-// var phantom = {
-//   display: "block",
-//   padding: "20px",
-//   height: "40px",
-//   width: "100%",
-// };
-
-// function Footer({ children }) {
-//   return (
-//     <div>
-//       <div style={phantom} />
-//       <div style={style}>{children}</div>
-//     </div>
-//   );
-// }
-
 export default Footer;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,13 +4,6 @@ import "../index.css";
 const Intro = () => {
   return (
     <div className="intro font-serif">
-      {/* <div class="grid grid-cols-2 gap-2">
-        <div>
-          <img src="/public/think.png"></img>
-        </div>
-
-        <div>6</div>
-      </div> */}
       Hello, Thanks for visiting! This site consists a list of good student
       opportunities (for developers and programmers) that the contributors found
       mining through the web. You can learn and upskill yourself by associating
