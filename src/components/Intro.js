fcde62dda5bf6a3f9568ae5b16c5d133b948671d
Surajv <vsuraj2405@gmail.com>
2021-04-02 21:22:44 +0530

    added intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../index.css";
+
+const Intro = () => {
+  return (
+    <div className="intro font-serif">
+      {/* <div class="grid grid-cols-2 gap-2">
+        <div>
+          <img src="/public/think.png"></img>
+        </div>
+
+        <div>6</div>
+      </div> */}
+      Hello, Thanks for visiting! This site consists a list of good student
+      opportunities (for developers and programmers) that the contributors found
+      mining through the web. You can learn and upskill yourself by associating
+      with them. Feel free to fork and contribute to this project.
+    </div>
+  );
+};
+
+export default Intro;
