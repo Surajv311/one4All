81f2ce07a7f22556307c9e4b6b155f21461a3973
Surajv <vsuraj2405@gmail.com>
2021-04-03 15:17:50 +0530

    horizontal rule ~line break

---
+++ src/components/Horizontalrule.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Horizontalrule = () => {
+  return (
+    <div>
+      <div class="horizontal_rule"></div>
+    </div>
+  );
+};
+
+export default Horizontalrule;
