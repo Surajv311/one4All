00318c02ccc0cf6b05c7e6604364911c2b66132c
Surajv <vsuraj2405@gmail.com>
2021-04-03 17:29:35 +0530

    add. comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <header class="font-serif">
         <h1>oneForAll (One point for all resources and opportunities)</h1>
+        {/* html tag ... <header>.. */}
       </header>
 
       <Intro />
