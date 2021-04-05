30d5ac3b7d9e14a3c70893de597e41139aa5a599
Surajv <vsuraj2405@gmail.com>
2021-04-05 21:23:02 +0530

    App -> Main.js...

---
+++ src/Main.js
@@ -0,0 +1,34 @@
+import "./Main.css";
+import React from "react";
+import Table from "./components/Table";
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
+import Ribbon from "./components/Ribbon";
+import Line from "./components/Line.js";
+function Main() {
+  return (
+    <div className="App">
+      <header class="font-serif">
+        <h1>oneForAll (One point for all resources and opportunities)</h1>
+        {/* html tag ... <header>.. */}
+      </header>
+      <Ribbon />
+      <Intro />
+      <br></br>
+      <h1>Opportunities for all</h1>
+      <br></br>
+      <Table />
+
+      <Line />
+      <Line />
+      <h1>Opportunities for all</h1>
+      <br></br>
+      <Table />
+      <Table />
+      <Table />
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
