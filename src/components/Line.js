2b4196aa7546231969912773f01d93ef9c03bbe9
Surajv <vsuraj2405@gmail.com>
2021-04-05 21:22:03 +0530

    renamed file

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "../App.css";
+import "../Main.css";
 
-const Horizontalrule = () => {
+const Line = () => {
   return (
     <div>
       <div class="horizontal_rule"></div>
@@ -9,4 +9,4 @@ const Horizontalrule = () => {
   );
 };
 
-export default Horizontalrule;
+export default Line;
