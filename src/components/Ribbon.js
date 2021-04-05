6d7300a93133a0b113d723bc00aa067ca783065a
Surajv <vsuraj2405@gmail.com>
2021-04-05 21:20:50 +0530

    shifted ribbon to component

---
+++ src/components/Ribbon.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Ribbon = () => {
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css"
+      />
+
+      <a
+        class="github-fork-ribbon"
+        href="https://github.com/Surajv311/oneForAll"
+        data-ribbon="Fork me on GitHub"
+        title="Fork me on GitHub"
+      >
+        Fork me on GitHub
+      </a>
+    </div>
+  );
+};
+
+export default Ribbon;
