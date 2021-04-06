9fa06b558d06bf448dc1ff8634f86ea3d96f8a23
Surajv <vsuraj2405@gmail.com>
2021-04-07 00:41:09 +0530

    added externship programs

---
+++ src/components/forAll/externship/Data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Major League Hacking (MLH) Externship ",
+    link: "https://fellowship.mlh.io/programs/externship",
+    info:
+      "Applications usually open twice a year for Summer and Spring. High School/Bootcamp/Non-Traditional students can apply. Limited number of non-student professionals are also selected.",
+  },
+  {
+    id: 2,
+    title: " Crio Winter of Doing ",
+    link: "https://www.crio.do/crio-winter-of-doing/",
+    info:
+      "Working professionals or student developers in their final year of engineering can go for it.",
+  },
+  {
+    id: 3,
+    title: "Github Externship",
+    link: "https://github-externships.github.io/externship/",
+    info:
+      "The GitHub Externship is a 90 days fellowship program for pre-final year students of GitHub Campus Partner schools.",
+  },
+];
