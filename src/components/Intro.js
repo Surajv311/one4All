fbbdd01b1f94c0963389e861936552a28df525d7
Surajv <vsuraj2405@gmail.com>
2021-04-06 15:16:17 +0530

    em tags in line...

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,9 +6,11 @@ const Intro = () => {
     <div className="intro font-serif">
       Hello, Thanks for visiting! This site consists a list of good student
       opportunities and resources (for developers and programmers) that the
-      contributors found mining through the web. You can learn and upskill
-      yourself by associating with them. Feel free to fork and contribute to
-      this project.
+      contributors found mining through the web. It basically has 3 sections.
+      The first lists out the <em>opportunities for everyone</em>, then{" "}
+      <em>opportunities exclusively for women</em> and lastly,{" "}
+      <em>resources</em>. You can learn and upskill yourself by associating with
+      them. Feel free to fork and contribute to this project.
     </div>
   );
 };
