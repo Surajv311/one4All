fe713d39d5ef933b14e2d41e97736d2b03b1890d
Surajv <vsuraj2405@gmail.com>
2021-04-07 14:02:33 +0530

    women scholarship progs.

---
+++ src/components/forWomen/scholarship/Data.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    id: 1,
+    title: "Nutanix Heart Women in Tech",
+    link: "https://www.nutanix.com/scholarships",
+    info:
+      "Female students from the countries (US, France, Netherland, India, Serbia), are awarded grants for their tuition fees.",
+  },
+  {
+    id: 2,
+    title: " D.E Shaw DESIS Ascend Fellowship",
+    link: "https://fellowships.deshaw.com/",
+    info: "  You'll be given scholarship amount if selected.",
+  },
+
+  {
+    id: 3,
+    title: "Generation Google Scholarship",
+    link:
+      "https://buildyourfuture.withgoogle.com/scholarships/generation-google-scholarship-apac/",
+    info:
+      "Applications usually open in March for APAC region. Deserving female candidates in their freshmen/sophomore year are awarded grants.",
+  },
+  {
+    id: 4,
+    title: " Adobe India Women-in-Technology Scholarship ",
+    link:
+      "https://research.adobe.com/adobe-india-women-in-technology-scholarship/",
+    info: "Applications usually open in August. ",
+  },
+  {
+    id: 5,
+    title: " Google Women Techmakers Engineering Fellows (WTEF)",
+    link: "https://we.talentsprint.com/",
+    info:
+      "Applications usually open in March. Deserving female candidates in their freshmen year are awarded grants and mentorship.",
+  },
+  {
+    id: 6,
+    title: "Anitab.Org Grace Hopper Celebration ",
+    link: "https://ghc.anitab.org/attend/scholarships/academics/",
+    info:
+      " If selected you'll get a sponsored 3 day Trip to USA to attend GHC and also career opportunities at Tech giants.",
+  },
+];
---
+++ src/components/forWomen/scholarship/Table.js
@@ -0,0 +1,78 @@
+import React from "react";
+import DataTable from "react-data-table-component";
+import Card from "@material-ui/core/Card";
+import data from "./Data";
+import "components/Table.css";
+
+const all_data = data.map((e) => ({
+  ...e,
+}));
+
+const Links = (e) => (
+  <div className="changeColor">
+    <a href={e.link}>Link</a>
+  </div>
+);
+const Infos = (e) => <p>{e.info}</p>;
+
+const columns = [
+  {
+    name: "Name",
+    selector: "title",
+    wrap: true,
+    center: 1,
+  },
+  {
+    name: "Info",
+    // selector: "info",
+    cell: (d) => <Infos {...d} />,
+    wrap: true,
+    center: 1,
+  },
+  {
+    name: "Link",
+    // selector: "link",
+    cell: (d) => <Links {...d} />,
+    // grow: 2,
+    center: 1,
+    wrap: true,
+  },
+];
+
+const customStyles = {
+  rows: {
+    style: {
+      minHeight: "90px",
+      fontSize: "16px",
+    },
+  },
+  headCells: {
+    style: {
+      paddingLeft: "8px",
+      paddingRight: "8px",
+      fontSize: "18px",
+    },
+  },
+  cells: {
+    style: {
+      paddingLeft: "8px",
+      paddingRight: "8px",
+    },
+  },
+};
+
+function Table() {
+  return (
+    <div className="font-serif table">
+      <Card>
+        <h1 class="alignset"> Scholarship Programs</h1>
+        <DataTable
+          columns={columns}
+          data={all_data}
+          customStyles={customStyles}
+        />
+      </Card>
+    </div>
+  );
+}
+export default Table;
