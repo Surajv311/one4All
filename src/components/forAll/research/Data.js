bac2cdaafa77d2184e7867855b15904b56397fb5
Surajv <vsuraj2405@gmail.com>
2021-04-07 13:37:10 +0530

    populated research component

---
+++ src/components/forAll/research/Data.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    title:
+      "Follow the link to get a list of research internship opportunities/scholarships. ",
+    link:
+      "https://github.com/Uncodedtech/Research-Internships-for-Undergraduates",
+    info: " (Check the website) ",
+  },
+];
