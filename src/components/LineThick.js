a7b2a96f5f9d7b65ac1cf42b1b7d74a05bf77dac
Surajv <vsuraj2405@gmail.com>
2021-04-09 01:15:43 +0530

    added thick line

---
+++ src/components/LineThick.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Main.css";
+
+const LineThick = () => {
+  return (
+    <div>
+      <div class="thick_horizontal_rule"></div>
+      <div class="thick_horizontal_rule"></div>
+    </div>
+  );
+};
+
+export default LineThick;
