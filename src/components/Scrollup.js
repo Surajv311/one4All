618ff7014dd68c6d3449f12983eeac17e99b2a60
Surajv <vsuraj2405@gmail.com>
2021-04-09 13:36:58 +0530

    fixed rule.. for scroll

--- src/components/Scrollup.js
+++ src/components/Scrollup.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ScrollToTop from "react-scroll-up";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+
+const customStyle = {
+  position: "fixed",
+  bottom: 30,
+  right: 10,
+  cursor: "pointer",
+  transitionDuration: "0.2s",
+  transitionTimingFunction: "linear",
+  transitionDelay: "0s",
+};
 
 const Scrollup = () => {
   return (
     <div>
-      <ScrollToTop showUnder={160}>
-        <span>UP</span>
+      <ScrollToTop style={customStyle} showUnder={160}>
+        <ArrowUpwardIcon style={{ fontSize: 35 }} />
       </ScrollToTop>
     </div>
   );
