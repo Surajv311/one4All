54f7e5530c487f63301cfb87af3cb0135bf942e3
Surajv <vsuraj2405@gmail.com>
2021-04-13 23:53:44 +0530

    line for choices added...

---
+++ src/components/IntroChoices.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+import "../fonts.css";
+
+const IntroChoices = () => {
+  return (
+    <div className="intro class_nun ">
+      It has <strong>3 sections</strong> namely: Opportunities
+      <span class="wordcol"> for everyone</span>, Opportunities
+      <span class="wordcol"> exclusively for women</span> & Resources.
+    </div>
+  );
+};
+
+export default IntroChoices;
