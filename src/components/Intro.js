c785ca72ab4053a1de89fbb71099ab209df9da04
Surajv <vsuraj2405@gmail.com>
2021-04-13 23:53:27 +0530

    word fix

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,8 @@ const Intro = () => {
   return (
     <div className="intro class_nun">
       Looking for resources? roadmaps? opportunities? Welcome to
-      <strong> one4All</strong>. One place for everything. The site consists a
-      list of good student opportunities and resources (
+      <strong> one4All</strong>. One place for everything. The site consists of
+      a list of good student opportunities and resources (
       <strong>for student developers & programmers</strong>).
     </div>
   );
