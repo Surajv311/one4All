8db61dfa90387ce8699e1ea7296594b19b129880
Surajv <vsuraj2405@gmail.com>
2021-04-13 11:00:51 +0530

    combined data

---
+++ src/components/Data.js
@@ -0,0 +1,521 @@
+// OPPORTUNITIES FOR ALL DATA
+
+const campusExpertData = [
+  {
+    id: 1,
+    title: "Microsoft Learn Student Ambassador",
+    link: "https://studentambassadors.microsoft.com/",
+    info:
+      "Students in any year of their undergrad course can apply. Applications are open throughout the year.",
+  },
+  {
+    id: 2,
+    title: " Github Campus Expert",
+    link: "https://apply.githubcampus.expert/",
+    info: "  Applications open twice a year in Feb and August.",
+  },
+  {
+    id: 3,
+    title: "GeeksForGeeks Campus Ambassador",
+    link:
+      "https://www.geeksforgeeks.org/campus-ambassador-program-by-geeksforgeeks/",
+    info:
+      " Sophomore and Prefinal year students can apply. Applications open in around May/June. ",
+  },
+  {
+    id: 4,
+    title: " HackerEarth University Ambassador Program",
+    link: "https://www.hackerearth.com/university/",
+    info: "(Check the website for updates)",
+  },
+  {
+    id: 5,
+    title: " Hackerrank Campus Ambassador  ",
+    link: "https://www.hackerrank.com/campus-ambassador-program",
+    info: "(Check the website for updates)",
+  },
+  {
+    id: 6,
+    title: " Cisco Devnet Campus Ambassador ",
+    link: "https://developer.cisco.com/site/student/",
+    info: "(Check the website for updates)",
+  },
+  {
+    id: 7,
+    title: "Intel Software Innovator Program ",
+    link:
+      "https://devmesh.intel.com/member-programs/intel-software-innovator-program#:~:text=Intel%20Software%20Innovator%20Program%20The%20Intel%C2%AE%20Software%20Innovator,demonstration%20opportunities%20at%20industry%20events%20and%20developer%20gatherings.",
+    info: "(Check the website for updates)",
+  },
+  {
+    id: 8,
+    title: " AWS Educate Student Ambassador ",
+    link:
+      "https://aws.amazon.com/education/awseducate/student-ambassador-program/",
+    info: "(Check the website for updates)",
+  },
+  {
+    id: 9,
+    title: " Alexa student influencer ",
+    link: "https://developer.amazon.com/en-IN/alexa/alexa-student-influencer",
+    info: "(Check the website for updates)",
+  },
+];
+
+const cpData = [
+  {
+    id: 1,
+    title: "Google Hashcode ",
+    link: "https://codingcompetitions.withgoogle.com/hashcode",
+    info:
+      "Applications usually open in the month of January. It's a team based competitive programming competition.",
+  },
+  {
+    id: 2,
+    title: "Google code jam ",
+    link: "https://codingcompetitions.withgoogle.com/codejam",
+    info: " Applications usually open in the month of February.",
+  },
+  {
+    id: 3,
+    title: "Google Kickstart",
+    link: "https://codingcompetitions.withgoogle.com/kickstart",
+    info:
+      "It's hosted 8 times a year and the registration for the first competition (Round A) usually begins in February.",
+  },
+  {
+    id: 4,
+    title: "Facebook Hackercup",
+    link: "https://www.facebook.com/codingcompetitions/hacker-cup",
+    info: "Applications usually open in the month of July.",
+  },
+  {
+    id: 5,
+    title: "ACM ICPC",
+    link: "https://icpc.global/",
+    info:
+      " It's a team based competition. Registrations usually start in winter. The dates for different regions for selections may vary.",
+  },
+  {
+    id: 6,
+    title: "Topcoder Open",
+    link: "https://www.topcoder.com/community/member-programs/topcoder-open",
+    info: "It has a design track and a programming track for the tournament.",
+  },
+];
+
+const externshipData = [
+  {
+    id: 1,
+    title: "MLH Externship ",
+    link: "https://fellowship.mlh.io/programs/externship",
+    info:
+      "Applications usually open twice a year for Summer and Spring. Students (& limited number of non-student professionals) are selected.",
+  },
+  {
+    id: 2,
+    title: " Crio Winter of Doing ",
+    link: "https://www.crio.do/crio-winter-of-doing/",
+    info:
+      "Working professionals or student developers in their final year of engineering can go for it.",
+  },
+  {
+    id: 3,
+    title: "Github Externship",
+    link: "https://github-externships.github.io/externship/",
+    info:
+      "The GitHub Externship is a 90 days fellowship program for pre-final year students of GitHub Campus Partner schools.",
+  },
+];
+
+const hackathonsData = [
+  {
+    id: 1,
+    title: "Microsoft Imagine Cup ",
+    link: "https://imaginecup.microsoft.com/en-us/Events?id=0",
+    info: " (Check the website) ",
+  },
+  {
+    id: 2,
+    title: "JP Morgan Code for Good",
+    link:
+      "https://careers.jpmorgan.com/US/en/students/programs/code-for-good?search=&tags=location__AsiaPacific__India",
+    info:
+      " Initial screening is a coding round followed by a hackathon. Applications usually open in Feb for APAC region.",
+  },
+  {
+    id: 3,
+    title: "Smart India Hackathon ",
+    link: "https://www.sih.gov.in/",
+    info:
+      "It has 2 editions- software and hardware & covers a wide variety of themes to build projects.",
+  },
+  {
+    id: 4,
+    title: "Pennapps Hackathon",
+    link: "https://pennapps.com/",
+    info: "(Check the website)",
+  },
+  {
+    id: 5,
+    title: "Hackmit ",
+    link: "https://hackmit.org/",
+    info: " (Check the website)",
+  },
+  {
+    id: 6,
+    title: "MLH Explorer ",
+    link: "https://fellowship.mlh.io/programs/explorer",
+    info:
+      "You'll be collaborating in small groups with other developers on a series of short hackathon sprints.",
+  },
+  {
+    id: 7,
+    title: " Apple's Swift Student Challenge",
+    link: "https://developer.apple.com/wwdc20/swift-student-challenge/",
+    info: " (Check the website) ",
+  },
+];
+
+const miscData = [
+  {
+    id: 1,
+    title: "Google STEP Internship ",
+    link: "https://buildyourfuture.withgoogle.com/programs/step/",
+    info:
+      "  A chance to intern at Google. It's only for first and second year undergrad students. Google India STEP intern applications usually open in November. ",
+  },
+  {
+    id: 2,
+    title: " Palantir Technology Scholarship",
+    link: "https://www.palantir.com/students/scholarship/",
+    info:
+      " It awards grants to students to support their education. Application usually opens in March.",
+  },
+  {
+    id: 3,
+    title: "Ethereum India Scholarship ",
+    link: "https://eif2.devfolio.co/",
+    info:
+      " It's an 8-week mentor led program for those interested to dive deep into the ethereum ecosystem. ",
+  },
+  {
+    id: 4,
+    title: " Kaggle Contests ",
+    link: "https://www.kaggle.com/",
+    info: "It hosts tons of Data Science contests every month.",
+  },
+  {
+    id: 5,
+    title: "  Codechef's Go For Gold initiative  ",
+    link: "https://www.codechef.com/goforgold",
+    info:
+      "Cash prize will be awarded to any Indian team that secures a world rank lower than (current best) 18 in ICPC. ",
+  },
+  {
+    id: 6,
+    title: " Udacity Scholarship ",
+    link: "https://www.udacity.com/scholarships",
+    info: "It offers nanodegree programs to deserving candidates.",
+  },
+  {
+    id: 7,
+    title: " Google Venkat Scholarship",
+    link:
+      "https://buildyourfuture.withgoogle.com/scholarships/venkat-panchapakesan-memorial-scholarship/",
+    info:
+      "Selected students will receive a financial grant towards tuition expenses, and will be invited to visit the YouTube headquarters in USA.",
+  },
+  {
+    id: 8,
+    title: " Samsung PRISM Programme ",
+    link: "https://www.samsungprism.com/",
+    info:
+      "Student and Professor form a team to work on real-world projects assigned by Samsung (Bengaluru). Also get to interact with industry mentors.",
+  },
+  {
+    id: 9,
+    title: " Google Nonprofit Marketing Immersion ",
+    link:
+      "https://www.google.com/grants/get-help/nonprofit-marketing-immersion/",
+    info:
+      "A challenge for those interested in digital marketing. The applications are opened round the year.",
+  },
+  {
+    id: 10,
+    title: "NASA Space Apps Hackathon",
+    link: "https://www.spaceappschallenge.org/",
+    info:
+      "Space Apps inspires communities to come together, think intensely, and create solutions to important problems. Event usually happens in Sept-Oct.",
+  },
+];
+
+const opensourceData = [
+  {
+    id: 1,
+    title: "(Follow the link)",
+    link: "https://github.com/Uncodedtech/Open-Source-Programs",
+    info: " (Check the website) ",
+  },
+];
+
+const researchData = [
+  {
+    id: 1,
+    title: "(Follow the link) ",
+    link:
+      "https://github.com/Uncodedtech/Research-Internships-for-Undergraduates",
+    info: " (Check the website) ",
+  },
+];
+
+/////////////////////////////////////////////////////////////////////////////
+
+// OPPORTUNITIES FOR WOMEN DATA
+
+const diversityhireData = [
+  {
+    id: 1,
+    title: "Flipkart Diversity Hiring",
+    link: "https://www.flipkartcareers.com/#!/joblist",
+    info: "(Check the website)",
+  },
+  {
+    id: 2,
+    title: "Flipkart Runway",
+    link: "https://dare2compete.com/o/flipkart-runway-flipkart-157441",
+    info: "Deserving candidates get a chance to intern at Flipkart.",
+  },
+  {
+    id: 3,
+    title: "Amazon WOW Internship",
+    link: "https://www.amazon.jobs/en/teams/internships-for-students",
+    info:
+      "A 6-months internship opportunity at Amazon for deserving candidates.",
+  },
+];
+
+const hackathonsWomenData = [
+  {
+    id: 1,
+    title: "Linkedin Wintathon",
+    link: "https://wintathon2020.splashthat.com/",
+    info:
+      " All prefinal & final year BTech/BE Women Students or a 1st / 2nd Year M.Tech Women Students can apply.",
+  },
+  {
+    id: 2,
+    title: " Adobe CoDiva ",
+    link:
+      "https://www.firstnaukri.com/careers/customised/landingpage/adobe/codiva/index.html",
+    info:
+      " Winners get prizes and a chance to work at Adobe. Open to prefinal and final year candidates.",
+  },
+  {
+    id: 3,
+    title: "American Express Makeathon",
+    link: "https://american-express-makeathon-2021.hackerearth.com/",
+    info: "Winners get a chance to intern/full-time opportunity at Amex.",
+  },
+];
+
+const mentorshipData = [
+  {
+    id: 1,
+    title: "Wootech Mentorship",
+    link: "https://wootechnology.github.io/Website/",
+    info: "(Check the website)",
+  },
+  {
+    id: 2,
+    title: "Amazon ACMS Mentorship",
+    link:
+      "https://www.linkedin.com/pulse/amazon-acms-complete-guide-vrinda-goyal?trk=public_profile_article_view",
+    info:
+      "  It's a mentorship cum internship opportunity for deserving candidates.",
+  },
+
+  {
+    id: 3,
+    title: " twitter developHER ",
+    link: "https://developher2020india.splashthat.com/",
+    info:
+      "Applications usually open in July. A two-day program for sophomore and prefinal year women students with lots of learning events.",
+  },
+  {
+    id: 4,
+    title: "  Microsoft Codess India ",
+    link: "https://careers.microsoft.com/students/us/en/ind-programs",
+    info: " (Check the website) ",
+  },
+  {
+    id: 5,
+    title: " Intuit Augment Program ",
+    link: "https://www.surveymonkey.com/r/INTUIT_2020",
+    info:
+      "This mentorship program is designed to make women industry-ready. Applications usually open in January. ",
+  },
+  {
+    id: 6,
+    title: " Goldman Sachs WeTech Scholarship  ",
+    link: "https://www.iie.org/WeTech-GoldmanSachs",
+    info:
+      "Deserving candidates chosen from selected universities. Applications usually open in November.",
+  },
+  {
+    id: 7,
+    title: " Google #IamRemarkable Facilitator  ",
+    link: "https://iamremarkable.withgoogle.com/",
+    info: " (Check the website)",
+  },
+
+  {
+    id: 8,
+    title: "Women who code Mentorship ",
+    link: "https://www.womenwhocode.com/",
+    info: " (Check the website) ",
+  },
+];
+
+const programmingWomenData = [
+  {
+    id: 1,
+    title: "code jam to I/O for women",
+    link: "https://codingcompetitions.withgoogle.com/codejamio",
+    info: " It's similar to code jam, but it's exclusively for women.",
+  },
+  {
+    id: 2,
+    title: " HackerRank Women's CodeSprint ",
+    link: "https://www.hackerrank.com/womens-codesprint-5",
+    info: " (Check the website)",
+  },
+  {
+    id: 3,
+    title: "Goldman Sachs Women's CodeSprint",
+    link: "https://www.hackerrank.com/goldman-sachs-womens-codesprint",
+    info: "(Check the website)",
+  },
+  {
+    id: 4,
+    title: " SheCodes, Indeed",
+    link: "https://www.shecodesindeed.com/",
+    info:
+      " The contest is conducted in September. Check the website for more info.",
+  },
+];
+
+const scholarshipData = [
+  {
+    id: 1,
+    title: "Nutanix Heart Women in Tech",
+    link: "https://www.nutanix.com/scholarships",
+    info:
+      "Female students from the countries (US, France, Netherland, India, Serbia), are awarded grants for their tuition fees.",
+  },
+  {
+    id: 2,
+    title: " D.E Shaw DESIS Ascend Fellowship",
+    link: "https://fellowships.deshaw.com/",
+    info: "  You'll be given scholarship amount if selected.",
+  },
+
+  {
+    id: 3,
+    title: "Generation Google Scholarship",
+    link:
+      "https://buildyourfuture.withgoogle.com/scholarships/generation-google-scholarship-apac/",
+    info:
+      "Applications usually open in March for APAC region. Deserving candidates in their first/second year are awarded grants.",
+  },
+  {
+    id: 4,
+    title: " Adobe India Women-in-Technology Scholarship ",
+    link:
+      "https://research.adobe.com/adobe-india-women-in-technology-scholarship/",
+    info: "Applications usually open in August. ",
+  },
+  {
+    id: 5,
+    title: " Google Women Techmakers Engineering Fellows (WTEF)",
+    link: "https://we.talentsprint.com/",
+    info:
+      "Applications usually open in March. Deserving female candidates in their freshmen year are awarded grants and mentorship.",
+  },
+  {
+    id: 6,
+    title: "Anitab.Org Grace Hopper Celebration ",
+    link: "https://ghc.anitab.org/attend/scholarships/academics/",
+    info:
+      " If selected you'll get a sponsored 3 day Trip to USA to attend GHC and also career opportunities at Tech giants.",
+  },
+];
+
+///////////////////////////////////////////////////////////////////////////////
+
+// RESOURCES DATA
+
+const resourcesData = [
+  {
+    id: 1,
+    title: "Interview Preparation",
+    link: "https://github.com/Uncodedtech/Interviewprep",
+  },
+  {
+    id: 2,
+    title: "Design Resources",
+    link: "https://github.com/Uncodedtech/design-resources-for-developers",
+  },
+
+  {
+    id: 3,
+    title: "Jobs List",
+    link: "https://github.com/Uncodedtech/Jobslist",
+  },
+  {
+    id: 4,
+    title: "Project based learning",
+    link: "https://github.com/Uncodedtech/Project_based_learning",
+  },
+  {
+    id: 5,
+    title: " APIs & resources",
+    link: "https://github.com/Uncodedtech/apis_devops",
+  },
+  {
+    id: 6,
+    title: "Roadmaps & resources ",
+    link: "https://github.com/Uncodedtech/RoadmapsResources",
+  },
+  {
+    id: 7,
+    title: "Cheatsheets ",
+    link: "https://github.com/Uncodedtech/awesome-cheatsheets",
+  },
+  {
+    id: 8,
+    title: "Startup,CEO,Marketing",
+    link: "https://github.com/Uncodedtech/ceo",
+  },
+  {
+    id: 9,
+    title: "Miscellaneous Repositories",
+    link: "https://github.com/Uncodedtech/miscRepositories",
+  },
+];
+
+export {
+  campusExpertData,
+  cpData,
+  externshipData,
+  hackathonsData,
+  miscData,
+  opensourceData,
+  researchData,
+  diversityhireData,
+  hackathonsWomenData,
+  mentorshipData,
+  programmingWomenData,
+  scholarshipData,
+  resourcesData,
+};
